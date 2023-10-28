5c488245f2c6b84d540b9260eba7d3dae5059300
박성윤 <p49417875@gmail.com>
2023-10-28 20:28:14 +0900

    chat기능 완료

---
+++ main2.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const OpenAI = require("openai");
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+const openai = new OpenAI({
+    apiKey: 'api-key를 입력하세요',
+});
+
+app.get('/test', (요청, 응답) => {
+    응답.sendFile(__dirname + '/index.html')
+})
+
+app.post('/getResponse', async (req, res) => {
+    // 유저프롬프트를 html에서 받음
+    const userPrompt = req.body.userPrompt;
+
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{
+            "role": "user",
+            "content": userPrompt
+        }],
+        temperature: 1,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    })
+    res.send(response.choices[0].message.content);
+})
+
+
+
+app.listen(8080, () => {
+    console.log("server started");
+})
